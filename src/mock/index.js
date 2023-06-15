54d3341a3abfb500e01f0444688014e7129e5d71
sabamoradi <saba.moradi7719@gmail.com>
2023-06-15 15:17:10 +0330

    finish second section desktop

---
+++ src/mock/index.js
@@ -0,0 +1,62 @@
+export const suggestedFlight = [
+    {   
+        id:1,
+        special:true,
+        origin:'تهران',
+        destination:'مشهد',
+        lowerBound:'11 شهریور',
+        upperBound:'14 شهریور',
+        exactPrice:'۱,۴۵۰,۰۰۰',
+        priceWithSale:'۱,۳۷۰,۰۰۰'
+    },
+    {   
+        id:2,
+        special:true,
+        origin:'تهران',
+        destination:'کیش',
+        lowerBound:'11 شهریور',
+        upperBound:'18 شهریور',
+        exactPrice:'۱,۴۵۰,۰۰۰',
+        priceWithSale:'۱,۳۷۰,۰۰۰'
+    },
+    {   
+        id:3,
+        special:true,
+        origin:'تهران',
+        destination:'تبریز',
+        lowerBound:'11 شهریور',
+        upperBound:'14 شهریور',
+        exactPrice:'۱,۴۵۰,۰۰۰',
+        priceWithSale:'۱,۳۷۰,۰۰۰'
+    },
+    {   
+        id:4,
+        special:false,
+        origin:'تهران',
+        destination:'شیراز',
+        lowerBound:'11 شهریور',
+        upperBound:'14 شهریور',
+        exactPrice:'۱,۴۵۰,۰۰۰',
+        priceWithSale:'۱,۳۷۰,۰۰۰'
+    },
+    {   
+        id:5,
+        special:false,
+        origin:'تهران',
+        destination:'اصفهان',
+        lowerBound:'11 شهریور',
+        upperBound:'14 شهریور',
+        exactPrice:'۱,۴۵۰,۰۰۰',
+        priceWithSale:'۱,۳۷۰,۰۰۰'
+    },
+    {   
+        id:6,
+        special:false,
+        origin:'تهران',
+        destination:'یزد',
+        lowerBound:'11 شهریور',
+        upperBound:'14 شهریور',
+        exactPrice:'۱,۴۵۰,۰۰۰',
+        priceWithSale:'۱,۳۷۰,۰۰۰'
+    },
+]
--- src/views/Home/container/HomeSecond/index.js
+++ src/views/Home/container/HomeSecond/index.js
@@ -1,5 +1,6 @@
 import "./style.scss";
 import { PlaneFill } from "../../../../assets/icons";
+import { suggestedFlight } from "../../../../mock";
 
 const HomeSecond = () => {
   return (
@@ -8,21 +9,37 @@ const HomeSecond = () => {
         <h5>پروازهای پیشنهادی</h5>
 
         <ul className="h_second_data">
-          <li className="second_data_wrapper">
-            <div className="second_data_item">
-              <div className="second_data_right">
-                <div className="second_special">فروش ویژه</div>
-                <div className="second_text">
-                  <div className="second_text_top">
-                    <p>تهران</p>
-                    <PlaneFill />
-                    <p>مشهد</p>
+          {suggestedFlight.map((element) => {
+            return (
+              <li className="second_data_wrapper" key={element.id}>
+                <div className="second_data_item">
+                  <div className="second_data_right">
+                    {element.special && (
+                      <div className="second_special">فروش ویژه</div>
+                    )}
+                    <div className="second_text">
+                      <div className="second_text_top">
+                        <p>{element.origin}</p>
+                        <PlaneFill />
+                        <p>{element.destination}</p>
+                      </div>
+                      <div className="second_text_bottom">
+                        <p>{element.lowerBound}</p>
+                        <p>تا {element.upperBound}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="second_data_left">
+                    <p className={element.special && 'second_data_line'}>{element.special ? element.exactPrice : "قیمت از"}</p>
+                    <p>
+                      <strong>{element.priceWithSale}</strong>
+                      تومان
+                    </p>
                   </div>
                 </div>
-              </div>
-              <div className="second_data_left"></div>
-            </div>
-          </li>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
