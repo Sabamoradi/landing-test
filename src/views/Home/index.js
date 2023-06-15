438a98010dfe67517bc1bab13d19353c8512c6ab
sabamoradi <saba.moradi7719@gmail.com>
2023-06-15 12:02:15 +0330

    add home page first section

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,9 +1,14 @@
 import "./style.scss";
+const homeImage = require("../../assets/images/home/home-header.png");
 
 const Home = () => {
   return (
-    <>
-    </>
+    <div className="home-header_container">
+      <img src={homeImage} />
+      <div className="container home_white_card">
+        
+      </div>
+    </div>
   );
 };
 
